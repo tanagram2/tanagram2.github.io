d33bd327af3f71402122d15be1a84aef1a41b0ba
tanagram2 <tanagramfakename@gmail.com>
2025-11-20 02:09:04 -0500

    Create interactive-element.js

---
+++ js/interactive-element.js
@@ -0,0 +1,48 @@
+// /js/interactive-element.js
+export class InteractiveElement {
+    constructor(x, y, width, height) {
+        this.bounds = { x, y, width, height };
+        this.isHovered = false;
+        this.isFocused = false;
+        this.isDisabled = false;
+        this.isVisible = true;
+        this.zIndex = 0;
+        this.parent = null;
+    }
+    
+    containsPoint(x, y) {
+        return x >= this.bounds.x && 
+               x <= this.bounds.x + this.bounds.width && 
+               y >= this.bounds.y && 
+               y <= this.bounds.y + this.bounds.height;
+    }
+    
+    handleMouseMove(x, y) {
+        this.isHovered = this.containsPoint(x, y);
+    }
+    
+    handleClick(x, y) {
+        if (this.containsPoint(x, y) && !this.isDisabled) {
+            this.gainFocus();
+            return true;
+        }
+        return false;
+    }
+    
+    handleKeyDown(event) {
+        // To be implemented by subclasses
+    }
+    
+    gainFocus() {
+        this.isFocused = true;
+    }
+    
+    loseFocus() {
+        this.isFocused = false;
+    }
+    
+    draw(ctx) {
+        // To be implemented by subclasses
+        throw new Error('draw method must be implemented by subclass');
+    }
+}
