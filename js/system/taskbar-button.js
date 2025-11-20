5eef7718d21901429cfa2840de3cd4eee0084e78
tanagram2 <tanagramfakename@gmail.com>
2025-11-20 04:11:09 -0500

    Update taskbar-button.js

--- js/system/taskbar-button.js
+++ js/system/taskbar-button.js
@@ -1,9 +1,7 @@
-// /js/system/taskbar-button.js
 import { Button } from './button.js';
 
 export class TaskbarButton extends Button {
     constructor(x, y, width, height, text, onClick, options = {}) {
-        // Pass options for taskbar styling
         super(x + width / 2, y + height / 2, width, height, text, onClick, {
             backgroundColor: '#9e9e9e',
             hoverColor: '#bdbdbd', 
@@ -11,12 +9,11 @@ export class TaskbarButton extends Button {
             borderRadius: 5,
             font: '1.2rem Courier New',
             borderColor: '#666',
-            ...options // Allow overriding
+            ...options
         });
         
-        // Taskbar buttons are positioned by top-left corner, not center
         this.bounds.x = x;
         this.bounds.y = y;
-        this.originalX = x + width / 2; // Recalculate text center
+        this.originalX = x + width / 2;
     }
 }
