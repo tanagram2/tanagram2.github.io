2aef9d743695cfd337cc946a7c8f7f09e7e7041a
tanagram2 <tanagramfakename@gmail.com>
2025-11-20 01:42:45 -0500

    Create button.js

---
+++ js/button.js
@@ -0,0 +1,51 @@
+// /js/button.js
+class Button {
+    constructor(x, y, width, height, text, onClick, options = {}) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.text = text;
+        this.onClick = onClick;
+        
+        // Default styling
+        this.backgroundColor = options.backgroundColor || '#0f0';
+        this.textColor = options.textColor || '#000';
+        this.hoverColor = options.hoverColor || '#0c0';
+        this.font = options.font || '1.2rem Courier New';
+        
+        this.isHovered = false;
+    }
+
+    containsPoint(x, y) {
+        return x >= this.x - this.width / 2 && 
+               x <= this.x + this.width / 2 && 
+               y >= this.y - this.height / 2 && 
+               y <= this.y + this.height / 2;
+    }
+
+    handleMouseMove(x, y) {
+        this.isHovered = this.containsPoint(x, y);
+    }
+
+    handleClick(x, y) {
+        if (this.containsPoint(x, y)) {
+            this.onClick();
+            return true;
+        }
+        return false;
+    }
+
+    draw(ctx) {
+        // Draw button background
+        ctx.fillStyle = this.isHovered ? this.hoverColor : this.backgroundColor;
+        ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+        
+        // Draw button text
+        ctx.fillStyle = this.textColor;
+        ctx.font = this.font;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(this.text, this.x, this.y);
+    }
+}
