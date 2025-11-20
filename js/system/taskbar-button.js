5d0bb08644bca88ddea65c30401fdc7a0d8f1e7f
tanagram2 <tanagramfakename@gmail.com>
2025-11-20 02:22:13 -0500

    Create taskbar-button.js

---
+++ js/system/taskbar-button.js
@@ -0,0 +1,19 @@
+// /js/system/taskbar-button.js
+import { Button } from './button.js';
+
+export class TaskbarButton extends Button {
+    constructor(x, y, width, height, text, onClick) {
+        super(x + width / 2, y + height / 2, width, height, text, onClick, {
+            backgroundColor: '#9e9e9e',
+            hoverColor: '#bdbdbd', 
+            textColor: '#000',
+            borderRadius: 5,
+            font: '1.2rem Courier New',
+            borderColor: '#666'
+        });
+        
+        this.bounds.x = x;
+        this.bounds.y = y;
+        this.originalX = x + width / 2;
+    }
+}
