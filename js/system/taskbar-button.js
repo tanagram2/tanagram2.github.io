0c2f167b5db6c65d85cc00c52e9d0cc94e34131a
tanagram2 <tanagramfakename@gmail.com>
2025-11-20 04:17:01 -0500

    Update taskbar-button.js

--- js/system/taskbar-button.js
+++ js/system/taskbar-button.js
@@ -1,14 +1,13 @@
-import { Button } from './button.js';
-
+// In /js/system/taskbar-button.js - make M green for debugging
 export class TaskbarButton extends Button {
     constructor(x, y, width, height, text, onClick, options = {}) {
         super(x + width / 2, y + height / 2, width, height, text, onClick, {
-            backgroundColor: '#9e9e9e',
-            hoverColor: '#bdbdbd', 
-            textColor: '#000',
+            backgroundColor: '#9e9e9e',  // Gray background
+            hoverColor: '#bdbdbd',       // Lighter gray on hover  
+            textColor: '#0f0',           // GREEN text so we can see it!
             borderRadius: 5,
             font: '1.2rem Courier New',
-            borderColor: '#666',
+            borderColor: '#000',         // Black border
             ...options
         });
         
